460b0ce1391b896660e57902304b7bc3a24929a0
esb580 <esb580@gmail.com>
2024-04-29 23:15:58 -0700

    Working.

--- app.py
+++ app.py
@@ -9,15 +9,16 @@ app = Flask(__name__)
 with open('pack.json', 'r') as f:
     tarot_deck = json.load(f)["pack"]  # Access the list inside the dictionary
 
+
 # Shuffle the deck
 def shuffle_deck():
     shuffled_deck = copy.deepcopy(tarot_deck)
     random.shuffle(shuffled_deck)
     return shuffled_deck
 
+
 # Select cards from the shuffled deck
 def select_cards(deck, num_cards):
-    #print(deck)
     # Ensure num_cards is an integer
     if not isinstance(num_cards, int):
         raise ValueError("num_cards must be an integer")
@@ -34,12 +35,17 @@ def select_cards(deck, num_cards):
         orientation = "upright" if random.randint(0, 1) == 0 else "reversed"
         card["orientation"] = orientation.capitalize()
         card["image_url"] = base_url + "img/" + orientation + "/" + card["card_image"]
-        card.pop("card_meaning_upright", None)
-        card.pop("card_meaning_reversed", None)
-        card.pop("card_description_upright", None)
-        card.pop("card_description_reversed", None)
+
+        if orientation == "upright":
+            card.pop("card_meaning_reversed", None)
+            card.pop("card_description_reversed", None)
+        else:  # orientation is "reversed"
+            card.pop("card_meaning_upright", None)
+            card.pop("card_description_upright", None)
+
         card.pop("card_image", None)
     return selected_cards
+
 
 @app.route('/tarot/3card', methods=['GET'])
 def get_3card_reading():
@@ -47,11 +53,13 @@ def get_3card_reading():
     selected_cards = select_cards(shuffled_deck, 3)
     return jsonify(selected_cards)
 
+
 @app.route('/tarot/5card', methods=['GET'])
 def get_5card_reading():
     shuffled_deck = shuffle_deck()
     selected_cards = select_cards(shuffled_deck, 5)
     return jsonify(selected_cards)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
